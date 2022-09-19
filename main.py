35cdc0b69de2d5405875b1b1c594fbe668b0615f
Glazkova_Marina <glazkova.mv@bk.ru>
2022-09-19 13:43:06 +0300

    add task 2

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ print(s)
 temp = '{:.' + str(s) + 'f}'
 print(temp.format(num))
 
+#2
+def Factor(n):
+    list = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            list.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        list.append(n)
+    return list
+
+num = int(input("Введите число: "))
+print(Factor(num))
+
